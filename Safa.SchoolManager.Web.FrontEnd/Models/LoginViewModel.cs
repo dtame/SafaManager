5b6360ec23eb0e5928a2c118a105ce83e3888452
Domnique Tame <mdtame@gmail.com>
2018-04-07 19:13:06 -0400

    getting the full project

---
+++ Safa.SchoolManager.Web.FrontEnd/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Safa.SchoolManager.Web.FrontEnd.Models
+{
+    public class LoginViewModel
+    {
+        [Required( ErrorMessage="Email is required")]
+        [DataType(DataType.EmailAddress, ErrorMessage="Should be email address")]
+        public string Email { get; set; }
+
+        [Required( ErrorMessage="Password is required")]
+        [StringLength(255, ErrorMessage = "Must be between at least 5 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]        
+        public string Password { get; set; }
+    }
+}
